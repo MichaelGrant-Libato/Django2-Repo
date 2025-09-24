cd6e397d95a3dcb7d5a97264f043c546952f8851
LAB13_USER ON WS22 <L13X16W22@LabHCI.net>
2025-09-24 18:00:36 +0800

    feature(djangoApp): setup templates

--- projGit/projGit/settings.py
+++ projGit/projGit/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'projGit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/djangoApp'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
